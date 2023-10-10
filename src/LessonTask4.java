a40340be26ab30e6c5c1485fad1e61ff4ee8d972
name <nikasan4@gmail.com>
2023-10-10 19:38:24 +0200

    Add new system out println

--- src/LessonTask4.java
+++ src/LessonTask4.java
@@ -1,5 +1,6 @@
 public class LessonTask4 {
     public static void main(String[] args){
         System.out.println("Hello world and Git!");
+        System.out.println("Second commit to Git");
     }
 }
